b8c8eb849171742c36f4e6b3b88b00609f05de1d
DabinGreat <873601257@qq.com>
2020-06-21 21:37:28 +0800

    add 0003

---
+++ 1-50/0003.py
@@ -0,0 +1,13 @@
+# 0003
+# 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        st = {}
+        i, ans = 0, 0
+        for j in range(len(s)):
+            if s[j] in st:
+                i = max(st[s[j]], i)
+            ans = max(ans, j-i+1)
+            st[s[j]] = j + 1
+        return ans
