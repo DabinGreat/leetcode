428fd0f14791b147dbdf0e92437b7c0f8047872d
dabincheng <873601257@qq.com>
2020-07-03 22:32:46 +0800

    add 0006

--- 1-50/0006.py
+++ 1-50/0006.py
@@ -10,7 +10,7 @@
 # L   C   I   R
 # E T O E S I I G
 # E   D   H   N
-# 之后，你的输出需要从左往右逐行读 取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+# 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
 
 
 class Solution:
